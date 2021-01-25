53ee86faa8d9e1afe41e9eb5c69d92d64fd5e97e
Ester Aguilar <eaguilar3110@gmail.com>
2021-01-25 19:31:04 +0100

    empezamos

---
+++ front-page.php
@@ -0,0 +1,6 @@
+<?php
+get_header();
+?>
+
+<h3>FRONT PAGE</h3>
+
---
+++ single.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+?>
+
+    <h1>Una entrada</h1>
+
+<?php the_post();?>
+<h2><?php the_title();?></h2>
+<br/></br>
