759840f8512c4135f2d0ab872408b98dc7f6b19e
Ester Aguilar <eaguilar3110@gmail.com>
2021-02-19 13:00:07 +0100

    menús

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
 
+function eaguilar_config(){
+        register_nav_menus(
+                array(
+                        'eaguilar_main_menu' => 'eaguilar menu principal',
+                )
+        );
+}
+
+add_action( 'after_setup_theme', 'eaguilar_config',  );
+
 function eaguilar_scripts(){
 
         wp_enqueue_style('my_css', get_stylesheet_uri(), array(), '1.0.0');
